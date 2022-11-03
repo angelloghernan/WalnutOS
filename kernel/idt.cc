b21e2a72928d3f85ab304216dcd1497dd839313e
Gianni Hernandez <a.gianni1234@gmail.com>
2022-11-02 23:48:24 -0400

    what

--- kernel/idt.cc
+++ kernel/idt.cc
@@ -3,7 +3,7 @@
 
 static Idt idt;
 static Idtr idtr;
-// extern void* isr_stub_table[];
+extern void* isr_stub_table[];
 
 extern "C" [[noreturn]] void exception_handler() {
     terminal.print_line("Unhandled exception!");
@@ -16,7 +16,7 @@ void Idt::init() {
     idtr.set_base(reinterpret_cast<usize>(&_idt[0]));
     idtr.set_limit(sizeof(IdtEntry) * MAX_NUM_DESCRIPTORS - 1);
     for (usize i = 0; i < _idt.len(); ++i) {
-        // _idt[i].set(isr_stub_table[i], 0x8E);
+        _idt[i].set(isr_stub_table[i], 0x8E);
     }
 
     __asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
