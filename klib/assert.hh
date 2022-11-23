8673ae5155568a512d91f322976181d281411d3f
Gianni Hernandez <a.gianni1234@gmail.com>
2022-11-23 11:18:27 -0800

    Assert system

--- klib/assert.hh
+++ klib/assert.hh
@@ -3,9 +3,6 @@
 
 inline void assert(bool const condition, char const* const file, i32 const line, 
                    char const* const function) {
-    (void) file;
-    (void) line;
-
     using namespace console;
     if (!condition) {
         terminal.print_line_color(Color::White, Color::Red, "Assertion failed! At ", function, " in ", file, ':', line);
