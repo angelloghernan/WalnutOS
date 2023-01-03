a0d02c2fa4ab9c64fef532118362e90ab2cc5367
Gianni Hernandez <a.gianni1234@gmail.com>
2023-01-03 12:35:14 -0500

    str: member variable style updated

--- klib/strings.hh
+++ klib/strings.hh
@@ -9,19 +9,19 @@
 struct str {
   public:
     template<usize S>
-    str(char const (&string)[S]) : string(string), size(S - 1) {}
-    constexpr str(char const* string, usize size) : string(string), size(size - 1) {}
+    str(char const (&string)[S]) : m_string(string), m_size(S - 1) {}
+    constexpr str(char const* string, usize size) : m_string(string), m_size(size - 1) {}
 
-    auto constexpr len() const -> usize { return size; }
+    auto constexpr len() const -> usize { return m_size; }
 
-    auto constexpr begin() const -> iterator<char const> { return iterator<char const>(string); }
-    auto constexpr end() const -> iterator<char const> { return iterator<char const>(string + size); }
+    auto constexpr begin() const -> iterator<char const> { return iterator<char const>(m_string); }
+    auto constexpr end() const -> iterator<char const> { return iterator<char const>(m_string + m_size); }
 
-    auto constexpr operator[](usize const idx) const -> char const& { return string[idx]; }
+    auto constexpr operator[](usize const idx) const -> char const& { return m_string[idx]; }
 
     auto constexpr get(usize const idx) const -> Option<char> {
-        if (idx < size) {
-            return Option<char>(string[idx]);
+        if (idx < m_size) {
+            return Option<char>(m_string[idx]);
         } else {
             return Option<char>();
         }
@@ -29,6 +29,6 @@ struct str {
 
 
   private:
-    char const* string;
-    usize size;
+    char const* m_string;
+    usize m_size;
 };
