190b74baf530d9e083ce8a777f7496ae2093977f
Gianni Hernandez <a.gianni1234@gmail.com>
2023-01-07 12:38:40 -0500

    Fix oversight with option

--- klib/option.hh
+++ klib/option.hh
@@ -18,6 +18,7 @@ class Option {
     auto constexpr unwrap() const -> T {
         return _val;
     }
+
     void constexpr assign(T const& value) {
         _val = value;
         _present = true;
@@ -28,6 +29,8 @@ class Option {
         _val = value; 
         _present = true; 
     }
+
+    bool constexpr operator==(Option<T>& value) { return _val == value.val; }
 
 
   private:
@@ -62,7 +65,7 @@ class Option<T&> {
     auto constexpr operator!() const -> bool { return !_val; }
     void constexpr operator=(T& value) { _val = &value; }
     void constexpr operator=(T const& value) { _val = &value; }
-    void constexpr operator==(Option<T&> const& value) { _val == value._val; }
+    bool constexpr operator==(Option<T&> const& value) { return _val == value._val; }
 
 
   private:
@@ -71,3 +74,29 @@ class Option<T&> {
 
 template <typename T>
 Option(T&) -> Option<T&>;
+
+template<>
+class Option<uptr> {
+  public:
+    constexpr Option(uptr const value) : _val(value) {}
+
+    constexpr Option() : _val(0) {}
+
+    auto constexpr none() const -> bool { return !_val; }
+    auto constexpr some() const -> bool { return _val; }
+    auto constexpr matches() const -> bool { return _val ? Some : None; }
+
+    auto constexpr unwrap() -> uptr& { return _val; }
+    auto constexpr unwrap() const -> uptr const& { return _val; }
+
+    void constexpr assign(uptr const& value) { _val = value; }
+
+    void constexpr make_none() { _val = 0; }
+
+    auto constexpr operator!() const -> bool { return !_val; }
+    void constexpr operator=(uptr const value) { _val = value; }
+    bool constexpr operator==(Option<uptr> const& value) { return _val == value._val; }
+    
+  private:
+    uptr _val;
+};
