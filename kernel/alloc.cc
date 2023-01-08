c74b783709b90eb96390a1dfd251dd997ea2818b
Gianni Hernandez <a.gianni1234@gmail.com>
2023-01-08 12:20:37 -0500

    Work on allocator

--- kernel/alloc.cc
+++ kernel/alloc.cc
@@ -4,11 +4,103 @@
 #include "../klib/array.hh"
 #include "../klib/result.hh"
 #include "../klib/bitmap.hh"
+#include "../klib/pagetables.hh"
 
 using namespace alloc;
 
+auto round_up_pow2(u32 num) -> u32;
+auto log2(u32 num) -> u8;
 
-auto BuddyAllocator::kalloc(usize size) -> uptr {
+BuddyAllocator::BuddyAllocator() {
+    for (u16 i = 0; i < m_block_sizes.len() - 1; ++i) {
+        m_block_next[i] = i + 1;
+        // Even in the case that i == 0, this still works since
+        // NULL_BLOCK is u16(-1) == 0xFFFFFFFF :)
+        m_block_prev[i] = i - 1;
+        m_block_sizes[i] = PAGESIZE;
+        m_block_is_free[i] = true;
+    }
+    
+    m_block_next.last() = NULL_BLOCK;
+    m_block_prev.last() = m_block_sizes.len() - 2;
+    m_block_sizes.last() = PAGESIZE;
+    m_block_is_free.last() = false;
+}
+
+auto BuddyAllocator::kalloc(usize const size) -> Option<uptr> {
+    auto const alloc_size = round_up_pow2(size);
+
+    // Max alloc size is 2^20
+    if (alloc_size & 0xFFF00000) {
+        return {};
+    }
+    
+    auto const start_idx = log2(alloc_size);
+
+    auto free_head_idx = -1_i16;
+    
+    for (auto i = start_idx; i < m_free_list_heads.len(); ++i) {
+        if (m_free_list_heads[i] != NULL_BLOCK) {
+            free_head_idx = m_free_list_heads[i];
+        }
+    }
+
+    if (free_head_idx == -1) {
+        return {};
+    }
+    
+    auto const block = pop_free_list(free_head_idx);
+    
+    m_block_sizes[block] = alloc_size;
+
+    return block * PAGESIZE + BLOCK_OFFSET;
+}
+
+void BuddyAllocator::kfree(uptr ptr) {
+    auto const block_idx = (ptr & (~0xFFF)) >> 12;
+    auto const block_size = m_block_sizes[block_idx];
+     
+
+}
+
+auto constexpr BuddyAllocator::pop_free_list(u16 const idx) -> u16 {
+    auto const head = m_free_list_heads[idx];
+    if (head != NULL_BLOCK) {
+        auto const next = m_block_next[head];
+        if (next != NULL_BLOCK) {
+            m_block_prev[next] = NULL_BLOCK;
+        }
+        m_block_prev[head] = NULL_BLOCK;
+        m_free_list_heads[idx] = next;
+    }
+    return head;
+}
+
+void constexpr BuddyAllocator::push_free_list(u16 const idx, u16 const block_idx) {
+    auto const head = m_free_list_heads[idx];
+    m_block_next[block_idx] = head;
+    m_free_list_heads[idx] = block_idx;
+    if (head != NULL_BLOCK) {
+        m_block_prev[head] = block_idx;
+    }
+}
+
+auto round_up_pow2(u32 num) -> u32 {
+    --num;
+    num |= num >> 1;
+    num |= num >> 2;
+    num |= num >> 4;
+    num |= num >> 8;
+    num |= num >> 16;
+    ++num;
     return 0;
 }
 
+auto log2(u32 num) -> u8 {
+    auto count = u8(-1);
+    while (num) {
+        num >>= 2;
+        ++count;
+    }
+    return count;
+}
--- kernel/alloc.hh
+++ kernel/alloc.hh
@@ -2,30 +2,48 @@
 #include "../klib/option.hh"
 #include "../klib/result.hh"
 #include "../klib/array.hh"
+#include "../klib/bitmap.hh"
 
 namespace alloc {
     class BuddyAllocator {
       public:
-        auto kalloc(usize size) -> uptr;
+        auto kalloc(usize size) -> Option<uptr>;
         void kfree(uptr ptr);
+        BuddyAllocator();
+        Bitmap<32> m_block_is_free;
       private:
         // Offset of where the block is located.
-        auto static constexpr BLOCK_OFFSET = 0;
+        auto static constexpr BLOCK_OFFSET = 0x1000;
 
         // Number of blocks. 
         // The [i]th block is located at memory location 4096 * i + BLOCK_OFFSET.
         auto static constexpr NUM_BLOCKS = 32;
+        
+        // Magic number representing no head in m_free_list_heads.
+        auto static constexpr NULL_BLOCK = 0xFFFF_u16;
 
         // Array[i] = the [i]th block's size
         Array<usize, 32> m_block_sizes;
 
         // An index 'pointing' to the next block in the free list, from the perspective of
         // the [i]th block.
-        Array<u8, 32> m_block_next;
+        Array<u16, 32> m_block_next;
+    
+        // An index 'pointing' to the previous block in the free list, from the perspective
+        // of the [i]th block.
+        Array<u16, 32> m_block_prev;
 
         // Array of indices 'pointing' to the head of each free list.
         // Each free list represents a block of some power of two, starting from
-        // 2^12 = 4096 bytes up to 2^19 bytes
-        Array<u8, 8> m_free_heads;
+        // 2^12 = 4096 bytes up to 2^20 bytes. If there is no head, then equals NULL_BLOCK.
+        Array<u16, 9> m_free_list_heads { 
+            0x0000, NULL_BLOCK, NULL_BLOCK, NULL_BLOCK, NULL_BLOCK, 
+            NULL_BLOCK, NULL_BLOCK, NULL_BLOCK, NULL_BLOCK 
+        };
+        
+
+        [[gnu::always_inline]] auto constexpr pop_free_list(u16 idx) -> u16;
+
+        [[gnu::always_inline]] void constexpr push_free_list(u16 list_idx, u16 block_idx);
     };
 };
