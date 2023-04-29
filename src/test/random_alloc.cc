49b96c1cf729c855ef560727886a79f5c091b36f
Gianni Hernandez <a.gianni1234@gmail.com>
2023-04-29 16:02:32 -0400

    random alloc test is slightly nicer

--- src/test/random_alloc.cc
+++ src/test/random_alloc.cc
@@ -18,12 +18,12 @@ extern "C" void kernel_main() {
     // Try every possible block size repeatedly
     for (i8 i = 1; i < 33; ++i) {
         auto num_blocks = 0;
-        terminal.print_line("Expecting ", i8(NUM_BLOCKS / alloc::round_up_pow2(i)), " blocks");
-        for (i8 j = 0; j < NUM_BLOCKS / (alloc::round_up_pow2(i)); ++j) {
+        terminal.print_line("Expecting ", i8(NUM_BLOCKS / alloc::round_up_pow2(i)), " block(s)");
+        for (i8 j = 0; j < i8(NUM_BLOCKS / (alloc::round_up_pow2(i))); ++j) {
             auto block = allocator.kalloc(PAGESIZE * i);
             assert(block.some(), "Allocator returned nothing");
             arr[num_blocks] = block.unwrap();
-            terminal.print_line("Allocator returned: ", (void*)block.unwrap());
+            terminal.print_line("Allocator returned: ", block.unwrap_as<void*>());
             ++num_blocks;
         }
 
