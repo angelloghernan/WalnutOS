21a7d5b91a20669b1348163bf8a5dcafbe06b8d4
Gianni Hernandez <a.gianni1234@gmail.com>
2023-08-05 20:00:57 -0700

    Add string splitting to str library

--- src/klib/strings.hh
+++ src/klib/strings.hh
@@ -1,20 +1,36 @@
 #pragma once
 #include "klib/int.hh"
+#include "klib/slice.hh"
+#include "klib/array.hh"
 #include "klib/option.hh"
 #include "klib/iterator.hh"
-#include "klib/slice.hh"
 #include "klib/type_traits.hh"
 
 namespace wlib {
     // str: A primitive type wrapping around string literals, allowing for easier iteration and slicing.
-    // When passing size, it is the size of the entire string, *including a null character*.
+    // When passing size, it is the size of the entire string, *including a null character*. Arrays are assumed
+    // to not include a null-terminator.
     struct str {
       public:
         template<usize S>
         constexpr str(char const (&string)[S]) : _string(string), _size(S - 1) {}
+
         constexpr str(char const* string, usize size) : _string(string), _size(size - 1) {}
+        
+        template<usize S>
+        constexpr str(Array<char, S> const& array) : _string(&array[0]), _size(S) {}
+
+        template<usize S>
+        constexpr str(Array<char, S> const& array, usize start, usize end) 
+                  : _string(&array[start]), _size(end - start) {}
 
         auto constexpr len() const -> usize { return _size; }
+
+        // Return a pointer to the first character in this string.
+        // Unsafe: Not to be used on string literals. Use as_const_ptr instead.
+        auto constexpr as_ptr() -> char* { return const_cast<char*>(&_string[0]); }
+
+        auto constexpr as_const_ptr() const -> char const* { return &_string[0]; }
 
         auto constexpr begin() const -> iterator<char const> { return iterator<char const>(_string); }
         auto constexpr end() const -> iterator<char const> { return iterator<char const>(_string + _size); }
@@ -60,9 +76,54 @@ namespace wlib {
         auto constexpr as_slice() const -> Slice<char const> {
             return Slice<char const>(_string, _size);
         }
+        class StrSplit {
+          public:
+            constexpr StrSplit(str& string, char ch) 
+                      : _string(string.as_const_ptr()), _pos(0), _end(string._size), _delimiter(ch) {}
+
+            [[nodiscard]] auto constexpr remainder() const -> str {
+                return str(_string + _pos, _end - _pos);
+            }
+
+            auto constexpr next() -> Option<str> {
+                if (_pos >= _end) {
+                    return Option<str>::None();
+                }
+
+                for (; _pos < _end && _string[_pos] == _delimiter; ++_pos) {}
+                
+                usize start = _pos;
+                usize end = _end;
+
+                for (auto i = _pos; i < _end; ++i) {
+                    if (_string[i] == _delimiter) {
+                        end = i;
+                        break;
+                    }
+                }
+
+                _pos = end + 1;
+
+                return Option<str>::Some(_string + start, end - start + 1);
+            }
+            
+            auto constexpr done() const -> bool {
+                return _pos >= _end;
+            }
+
+            char const* _string;
+            usize _pos;
+            usize _end;
+            char _delimiter;
+        };
+
+        [[nodiscard]] auto constexpr split(char ch) -> StrSplit {
+            return StrSplit(*this, ch);
+        }
 
       private:
         char const* _string;
         usize _size;
     };
+    
 };
