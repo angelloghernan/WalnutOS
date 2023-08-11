a36677c0741e4f0e9547e227243529d26e59e03e
Gianni Hernandez <a.gianni1234@gmail.com>
2023-08-11 16:48:45 -0700

    klib: Change ports to be inlined (header-file only)

--- src/klib/ports.cc
+++ src/klib/ports.cc
@@ -1,46 +1,4 @@
 #include "klib/ports.hh"
 
 namespace wlib::ports {
-    void outb(u16 port, u8 val) {
-        asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
-    }
-
-    void outw(u16 port, u16 val) {
-        asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
-    }
-
-    void outl(u16 port, u32 val) {
-        asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port));
-    }
-
-    auto inb(u16 port) -> u8 {
-        u8 ret;
-        asm volatile ( "inb %1, %0 " : "=a" (ret) : "Nd" (port));
-        return ret;
-    }
-
-    auto inw(u16 port) -> u16 {
-        u16 ret;
-        asm volatile ( "inw %1, %0 " : "=a" (ret) : "Nd" (port));
-        return ret;
-    }
-
-    auto inl(u16 port) -> u32 {
-        u32 ret;
-        asm volatile ( "inl %1, %0 " : "=a" (ret) : "Nd" (port));
-        return ret;
-    }
-
-    void insw(u16 port, uptr buffer, u32 count) {
-        asm volatile (
-            "rep insw"
-            : "+D"(buffer), "+c"(count)
-            : "d"(port)
-            : "memory"
-        );
-    }
-
-    void io_wait() {
-        outb(0x80, 0);
-    }
 }
--- src/klib/ports.hh
+++ src/klib/ports.hh
@@ -2,12 +2,46 @@
 #include "klib/int.hh"
 
 namespace wlib::ports {
-    void outb(u16 port, u8 val);
-    void outw(u16 port, u16 val);
-    void outl(u16 port, u32 val);
-    auto inb(u16 port) -> u8;
-    auto inw(u16 port) -> u16;
-    auto inl(u16 port) -> u32;
-    void insw(u16 port, uptr buffer, u32 count);
-    void io_wait();
+    inline void outb(u16 port, u8 val) {
+        asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    }
+
+    inline void outw(u16 port, u16 val) {
+        asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
+    }
+
+    inline void outl(u16 port, u32 val) {
+        asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port));
+    }
+
+    inline auto inb(u16 port) -> u8 {
+        u8 ret;
+        asm volatile ( "inb %1, %0 " : "=a" (ret) : "Nd" (port));
+        return ret;
+    }
+
+    inline auto inw(u16 port) -> u16 {
+        u16 ret;
+        asm volatile ( "inw %1, %0 " : "=a" (ret) : "Nd" (port));
+        return ret;
+    }
+
+    inline auto inl(u16 port) -> u32 {
+        u32 ret;
+        asm volatile ( "inl %1, %0 " : "=a" (ret) : "Nd" (port));
+        return ret;
+    }
+
+    inline void insw(u16 port, uptr buffer, u32 count) {
+        asm volatile (
+            "rep insw"
+            : "+D"(buffer), "+c"(count)
+            : "d"(port)
+            : "memory"
+        );
+    }
+
+    inline void io_wait() {
+        outb(0x80, 0);
+    }
 }; // namespace wlib
